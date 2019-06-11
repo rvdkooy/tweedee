3f5285c1da05acda79ab8ee29877b02f862e69c1
Ronald van der Kooij <ronald@sharpsolutions.nl>
2019-06-11 23:52:01 +0200

    removed redundant imageobject

--- src/objects/image.js
+++
@@ -1,9 +0,0 @@
-import GameObject from './gameObject';
-import imageBehaviour from '../behaviours/image';
-import Dimensions from './dimensions';
-
-class ImageObject extends GameObject {
-  
-}
-
-export default ImageObject;
--- src/objects/index.js
+++ src/objects/index.js
@@ -1,6 +1,5 @@
 export {default as Background} from './background';
 export {default as Dimensions} from './dimensions';
 export {default as GameObject} from './gameObject';
-export {default as ImageObject} from './image';
 export {default as Point} from './point';
 export {default as SpriteSheetObject} from './spritesheet';

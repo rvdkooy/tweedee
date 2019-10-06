84727cef2ec4ed79ad83599551be958550827536
Ronald van der Kooij <ronald@sharpsolutions.nl>
2019-10-06 16:45:50 +0200

    movement behaviour now supports all directions

--- src/behaviours/movement.js
+++ src/behaviours/movement.js
@@ -2,14 +2,18 @@ function movementBehaviour() {
     this.direction = null;
     this.speed = 0;
     this.easeInValue = 0;
+    this.deltaX = 0;
+    this.deltaY = 0;
     this.boundaries = null;
   
     this.move = function (direction, speed, options) {
       this.options = options || {};
       this.direction = direction;
-  
+      
       if (speed !== undefined && speed !== null) {
-        this.speed = speed;
+        this.speed = (this.options.easeIn) ? this.easeInValue : this.speed;
+        this.deltaX = (speed * Math.cos(direction * Math.PI / 180));
+        this.deltaY = -(speed * Math.sin(direction * Math.PI / 180));
       }
     }
   
@@ -42,21 +46,9 @@ function movementBehaviour() {
           this.easeInValue += 1;
         }
   
-        let speed = (this.options.easeIn) ? this.easeInValue : this.speed;
-        this.checkBoundary(speed);
-  
-        if (this.direction === 0) {
-          this.point.y = this.point.y - speed;
-        }
-        if (this.direction === 90) {
-          this.point.x = this.point.x + speed;
-        }
-        if (this.direction === 180) {
-          this.point.y = this.point.y + speed;
-        }
-        if (this.direction === 270) {
-          this.point.x = this.point.x - speed;
-        }
+        this.checkBoundary(this.speed);
+        this.point.x += this.deltaX;
+        this.point.y += this.deltaY;
       }
     }
   
